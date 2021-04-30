9139acb774b41c83d1ed066eafc053323710c854
hongj22 <52927259+hongj22@users.noreply.github.com>
2021-04-30 12:03:46 -0400

    Updated Piece.java

--- Piece.java
+++ Piece.java
@@ -1,4 +1,29 @@
+import java.util.*;
+
 public class Piece {
-    int r, c;//for location
-    int pts;//for point value
+
+	protected BoardLoc pos;
+	protected int pts;
+
+	protected ArrayList<BoardLoc> moves;
+
+	public Piece(int r, int c)
+	{
+		pos.r = r;
+		pos.c = c;
+		pos.piece = this;
+	}
+
+	public boolean move(BoardLoc location) {
+		pos.piece = null;
+		pos = location;
+
+		if(location.piece == null) {
+			location.piece = this;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
