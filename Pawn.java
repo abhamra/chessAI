32a85aff84c571ce05ddea565a8db9048af37414
hongj22 <52927259+hongj22@users.noreply.github.com>
2021-05-04 08:54:22 -0400

    pawn code so far
    Not finished, but here's a progress update!

--- Pawn.java
+++ Pawn.java
@@ -1,9 +1,37 @@
 public class Pawn extends piece{
-    
-    public Pawn(){
-        super();
+
+    protected int timesMoved;
+
+    public Pawn(int r, int c, boolean white) {
+        super(r, c, white);
+        timesMoved = 0;
     }//end constructor
 
-    
+    public void generateValidMoves(BoardLoc[][] board) {
+      moves = new ArrayList<BoardLoc>(); // YO, this is kinda not efficient, I will try to improve later...
+      if(timesMoved == 0) { // first move
+        if(white) {
+          moves.add(board[r - 2][c]);
+          moves.add(board[r - 1][c]);
+        }
+        else {
+          moves.add(board[r + 2][c]);
+          moves.add(board[r + 1][c]);
+        }
+      }
+      else { // already moved once or more
+        if(white) {
+          moves.add(board[r - 1][c]);
+        }
+        else {
+          moves.add(board[r + 1][c]);
+        }
+      }
+
+    }
+
+    public void move() {
+      
+    }
 
 }//end class
