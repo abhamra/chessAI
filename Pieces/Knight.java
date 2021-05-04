d1e60ed76206256cebef23ec5d5631cb9eae97e2
abhamra <33864884+abhamra@users.noreply.github.com>
2021-05-04 08:32:31 -0400

    created knight.java

---
+++ Pieces/Knight.java
@@ -0,0 +1,15 @@
+public class Knight extends Piece {
+    
+    protected BoardLoc pos;
+	protected int pts = 3;
+
+    public Knight(int r, int c){
+        super(r, c);
+
+    }//end constructor
+    
+    public void generateValidMoves(){
+
+    }//end func
+
+}//end class
