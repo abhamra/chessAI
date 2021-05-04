43d6458a0ca117fe234444277719d3e1038cfcca
hongj22 <52927259+hongj22@users.noreply.github.com>
2021-05-04 08:57:52 -0400

    replacing duplicate piece class

--- Pieces/Piece.java
+++ Pieces/Piece.java
@@ -4,20 +4,23 @@ public class Piece {
 
 	protected BoardLoc pos;
 	protected int pts;
+	protected boolean white; // if true, white -- if false, black
 
 	protected ArrayList<BoardLoc> moves;
 
-	public Piece(int r, int c)
+	public Piece(int r, int c, boolean white)
 	{
 		pos.r = r;
 		pos.c = c;
 		pos.piece = this;
 
+		this.white = white;
+
 		moves = new ArrayList<BoardLoc>();
 	}
 
 	public void generateValidMoves() { // OVERRIDE this method
-		
+
 	}
 
 	public boolean move(BoardLoc location) {
