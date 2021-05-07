700a08dae340d50e5d38823db992a359490aa528
hongj22 <52927259+hongj22@users.noreply.github.com>
2021-05-07 09:51:30 -0400

    Adding comments to Rook.java

--- Pieces/Rook.java
+++ Pieces/Rook.java
@@ -9,26 +9,26 @@ public class Rook extends Piece {
 
   public void generateValidMoves(BoardLoc[][] board) {
     super.moves = new ArrayList<BoardLoc>();
-    int[] dr = {0, 1, 0, -1};
-    int[] dc = {1, 0, -1, 0};
-    for(int i = 0; i < 4; i++) {
+    int[] dr = {0, 1, 0, -1}; // handles the four directions
+    int[] dc = {1, 0, -1, 0}; // handles the four directions
+    for(int i = 0; i < 4; i++) { // iterates through the four directions
       int nr = super.pos.r;
       int nc = super.pos.c;
       while(nr < board.length && nr >= 0
-         && nc < board[0].length && nc >= 0) {
+         && nc < board[0].length && nc >= 0) { // while edge has not been found
         nr += dr[i];
         nc += dc[i];
-        if(board[nr][nc].piece == null) {
-          super.moves.add(board[nr][nc]);
+        if(board[nr][nc].piece == null) { // if no piece in current tile
+          super.moves.add(board[nr][nc]); // just add it to valid moves
         }
         else if((board[nr][nc].piece.white && this.white)
-              || !board[nr][nc].piece.white && !this.white) {
-          break;
+              || !board[nr][nc].piece.white && !this.white) { // if same color
+          break; // don't consider as valid move and break immediately because you can't friendly fire
         }
         else if((board[nr][nc].piece.white && !this.white)
-              || !board[nr][nc].piece.white && this.white) {
-          super.moves.add(board[nr][nc]);
-          break;
+              || !board[nr][nc].piece.white && this.white) { // if diff color
+          super.moves.add(board[nr][nc]); // consider taking it
+          break; // then break cuz you can't move past the piece if you want to take it
         }
       }
     }
