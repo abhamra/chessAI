c75c01ac70b77e5bba396305f317eef630118531
abhamra <33864884+abhamra@users.noreply.github.com>
2021-05-07 10:08:49 -0400

    fixing movement for bishop and knight

--- Pieces/Bishop.java
+++ Pieces/Bishop.java
@@ -1,26 +1,27 @@
+import java.util.*;
 public class Bishop extends Piece {
     
     protected BoardLoc pos;
 	protected int pts = 3;
 
-    public Bishop(int r, int c){
-        super(r, c);
+    public Bishop(int r, int c, boolean white){
+        super(r, c, white);
 
     }//end constructor
     
     public void generateValidMoves(BoardLoc[][] board) {
-        super.moves = new ArrayList<BoardLoc>();
+        moves = new ArrayList<BoardLoc>();
         int[] dr = {1, -1, 1, -1};
         int[] dc = {1, 1, -1, -1};
         for(int i = 0; i < 4; i++) {
-          int nr = super.pos.r;
-          int nc = super.pos.c;
+          int nr = pos.r;
+          int nc = pos.c;
           while(nr < board.length && nr >= 0
              && nc < board[0].length && nc >= 0) {
             nr += dr[i];
             nc += dc[i];
             if(board[nr][nc].piece == null) {
-              super.moves.add(board[nr][nc]);
+              moves.add(board[nr][nc]);
             }
             else if((board[nr][nc].piece.white && this.white)
                   || !board[nr][nc].piece.white && !this.white) {
@@ -28,7 +29,7 @@ public class Bishop extends Piece {
             }
             else if((board[nr][nc].piece.white && !this.white)
                   || !board[nr][nc].piece.white && this.white) {
-              super.moves.add(board[nr][nc]);
+              moves.add(board[nr][nc]);
               break;
             }
           }
