baf38b18f39b39daa000c49b0946d0106840117b
abhamra <33864884+abhamra@users.noreply.github.com>
2021-05-15 14:01:07 -0400

    eval func

--- Game.java
+++ Game.java
@@ -18,11 +18,21 @@ public class Game {
       }
     }
 
-    player1 = new Player(board, true);
+    player1 = new Player(board, true);//white
     player2 = new Player(board, false);
 
     moveCount = 0;
   }
+
+  public int evaluateBoard(BoardLoc[][] board){
+    int totalEvaluation = 0;
+    for(int r = 0;r<board.length;r++){
+      for(int c = 0;c<board[0].length;c++){
+        totalEvaluation += board[r][c].relStrength;
+      }//end nested for
+    }
+    return totalEvaluation;
+  }//end func
 
   
 }
